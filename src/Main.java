6f71182304b0772e497e1be35a8c18e0b5b7a77a
Hazem Kiwan <hkiwan@itgsoftware.com>
2022-03-30 20:08:55 +0300

    Add Digram, Answers, and Read me files

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ public class Main {
 
 	public static void main(String[] args) {
 //		String s = "An 😀awesome 😃string 😄with a few 😉emojis!";
-		String s = "An 😀awesome 😃يوجد 😄with a few 😉بعض الوجوه!";
+		String s = "מחרוזת 😀מדהימה 😄עם כמה 😉אימוג'ים!";
+//		String s = "An 😀awesome 😃يوجد 😄with a few 😉بعض الوجوه!";
 		EmojiManager em = new EmojiManager(s);
 		em.displayString();
 		em.removeEmojis();
